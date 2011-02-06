d8acf0c3af323fc5ebea1af4ff10621fcc188a10
dave mankoff <mankyd@gmail.com>
2011-02-05 23:32:55 -0500

    remove server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-from twisted.application import internet, service
-
-import bartleby
-
-# 8007 is the port you want to run under. Choose something >1024
-application = service.Application('bartleby')
-factory = bartleby.BartlebyFactory()
-internet.TCPServer(9007, factory).setServiceParent(service.IServiceCollection(application))
-
